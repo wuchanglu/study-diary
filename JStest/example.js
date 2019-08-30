ab815b896452552acc0ae248e375917166732859
吴昌禄 <1761221407@qq.com>
2019-08-30 19:08:32 +0800

    host

--- JStest/example.js
+++ JStest/example.js
@@ -4,7 +4,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World！\n');
+res.end('Hello World');
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
