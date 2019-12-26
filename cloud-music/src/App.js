37d5802ef8175642c6fdf8bcdf8200e8b5740b41
吴昌禄 <1761221407@qq.com>
2019-12-26 17:37:49 +0800

    轮播图+列表 12/26

--- cloud-music/src/App.js
+++ cloud-music/src/App.js
@@ -4,15 +4,19 @@ import { IconStyle } from "./assets/iconfont/iconfont";
 import routes from "./routes/index.js";
 import { HashRouter } from "react-router-dom";
 import { renderRoutes } from "react-router-config"; //renderRoutes 读取路由配置转化为 Route 标签
+import { Provider } from "react-redux";
+import store from "./store/index";
 function App() {
   return (
-    <HashRouter>
-      <div>
-        <GlobalStyle></GlobalStyle>
-        <IconStyle></IconStyle>
-        {renderRoutes(routes)}
-      </div>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <div>
+          <GlobalStyle></GlobalStyle>
+          <IconStyle></IconStyle>
+          {renderRoutes(routes)}
+        </div>
+      </HashRouter>
+    </Provider>
   );
 }
 
--- cloud-music/src/application/Home/index.js
+++ cloud-music/src/application/Home/index.js
@@ -1,7 +1,8 @@
 //src/appliction/Home/index.js
 import React from "react";
 import { renderRoutes } from "react-router-config";
-import { Top } from "./style";
+import { Top, Tab, TabItem } from "./style";
+import { NavLink } from "react-router-dom";
 
 function Home(props) {
   const { route } = props;
@@ -12,6 +13,23 @@ function Home(props) {
         <span className="title">WebApp</span>
         <span className="iconfont search">&#xe62b;</span>
       </Top>
+      <Tab>
+        <NavLink className="link" to="/recommend" activeClassName="selected">
+          <TabItem>
+            <span> 推荐 </span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/singers" activeClassName="selected">
+          <TabItem>
+            <span> 歌手 </span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/rank" activeClassName="selected">
+          <TabItem>
+            <span> 排行榜 </span>
+          </TabItem>
+        </NavLink>
+      </Tab>
       {renderRoutes(route.routes)}
     </div>
   );
