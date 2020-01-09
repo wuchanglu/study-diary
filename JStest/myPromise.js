e7aa064d662fdbd402e12d18d1ac01a09c335885
吴昌禄 <1761221407@qq.com>
2020-01-09 18:36:00 +0800

    promise手写（未完成）

---
+++ JStest/myPromise.js
@@ -0,0 +1,108 @@
+// const PENDING = "pending";
+// const RESOLVED = "resolved";
+// const REJECTED = "rejected";
+// var dataList = [];
+// var thenList = [];
+// function MyPromise(fn) {
+//   const that = this;
+//   that.state = PENDING;
+//   that.value = null;
+//   that.resolvedCallbacks = [];
+//   that.rejectedCallbacks = [];
+
+//   // 待完善 resolve 和 reject 函数
+//   // 待完善执行 fn 函数
+//   function resolve(value) {
+//     dataList.push(that);
+//     // console.log(value);
+//     if (value instanceof MyPromise) {
+//       value.then(resolve, reject);
+//       //
+//       //
+//       return;
+//     }
+//     if (that.state === PENDING) {
+//       that.state = RESOLVED;
+//       that.value = value;
+//       that.resolvedCallbacks.map(cb => cb(that.value));
+//     }
+//     return that;
+//   }
+
+//   function reject(value) {
+//     if (that.state === PENDING) {
+//       that.state = REJECTED;
+//       that.value = value;
+
+//       that.rejectedCallbacks.map(cb => cb(that.value));
+//     }
+//   }
+//   try {
+//     fn(resolve, reject);
+//   } catch (e) {
+//     reject(e);
+//   }
+// }
+// MyPromise.prototype.then = function(onFulfilled, onRejected) {
+//   const that = this;
+//   thenList.push(that);
+//   onFulfilled = typeof onFulfilled === "function" ? onFulfilled : v => v;
+//   onRejected =
+//     typeof onRejected === "function"
+//       ? onRejected
+//       : r => {
+//           throw r;
+//         };
+//   if (that.state === PENDING) {
+//     that.resolvedCallbacks.push(onFulfilled);
+//     that.rejectedCallbacks.push(onRejected);
+//   }
+//   if (that.state === RESOLVED) {
+//     onFulfilled(that.value);
+//     console.log(that, onFulfilled(that.value));
+//   }
+//   if (that.state === REJECTED) {
+//     onRejected(that.value);
+//   }
+// };
+// var xx = "";
+// var p1 = new MyPromise(function(resolve) {
+//   setTimeout(() => {
+//     xx = new MyPromise(resolve => {
+//       resolve(5);
+//     });
+//     resolve(xx);
+//   }, 0);
+// });
+// p1.then(res => {
+//   //   console.log("final", res);
+// });
+
+// function test(name) {
+//   this.name = name;
+//   function sayName() {
+//     console.log("say", that.name);
+//   }
+//   if (o1) {
+//     //   console.log(this)
+//     var that = o1;
+//     this.doing(sayName);
+//   }
+// }
+// test.prototype.doing = function(fun) {
+//   var that = this;
+//   console.log(this,that)
+//   fun();
+// };
+// var o1 = new test("aaa");
+// var o2 = new test("bbb");
+var a = 11;
+function test() {
+  console.log(123);
+  console.log(a);
+}
+function c() {
+  var a = 2;
+  test();
+}
+c()
