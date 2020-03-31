178f22cfb35d65e57e8a00c99a89707f605ab7ba
吴昌禄 <1761221407@qq.com>
2020-03-31 15:44:53 +0800

    muban

---
+++ ssr/nuxt-test/pages/block.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="block">
+    <com-header></com-header>
+    <aside class="block__aside">
+      <nav class="nav">
+        <ul v-for="(item, index) in navList" :key="index">
+          <a>{{ item.title }}</a>
+          <li
+            :title="item.title"
+            v-for="(child, childIndex) in item.children"
+            :key="childIndex"
+          >
+            {{ child.title }}
+          </li>
+        </ul>
+      </nav>
+    </aside>
+    <section class="block__content">
+        <nuxt/>
+    </section>
+  </div>
+</template>
+
+<script>
+import comHeader from "../components/header";
+export default {
+  components: {
+    comHeader
+  },
+  data() {
+    return {
+      navList: [
+        {
+          title: "Node.js",
+          children: [
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程打算打开拉萨洪都拉斯肯定会拉萨撒",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            }
+          ]
+        },
+        {
+          title: "Node.js",
+          children: [
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            }
+          ]
+        },
+        {
+          title: "Node.js",
+          children: [
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            }
+          ]
+        },
+        {
+          title: "Node.js",
+          children: [
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程打算打开拉萨洪都拉斯肯定会拉萨撒",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            }
+          ]
+        },
+        {
+          title: "Node.js",
+          children: [
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            }
+          ]
+        },
+        {
+          title: "Node.js",
+          children: [
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            }
+          ]
+        },
+        {
+          title: "Node.js",
+          children: [
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程打算打开拉萨洪都拉斯肯定会拉萨撒",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            }
+          ]
+        },
+        {
+          title: "Node.js",
+          children: [
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            }
+          ]
+        },
+        {
+          title: "Node.js",
+          children: [
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            },
+            {
+              title: "node进程",
+              url: ""
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.block {
+  padding-top: 65px;
+  &__aside {
+    position: fixed;
+    z-index: 100;
+    top: 65px;
+    left: 0;
+    bottom: 0;
+    width: 260px;
+    background-color: transparent;
+    box-sizing: border-box;
+    transition: left 0.3s;
+    width: 300px;
+    padding-top: 50px;
+    border-right: 1px solid #ddd;
+    .nav {
+        height: 100%;
+        overflow-y: auto;
+        padding-bottom: 20px;
+      ul {
+        padding: 15px 24px 15px 58px;
+        a {
+          font-size: 18px;
+        }
+        li {
+          font-size: 15px;
+          padding: 10px 20px 0 10px;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          color: #444;
+          cursor: pointer;
+          &:hover {
+            color: @primary;
+          }
+        }
+      }
+    }
+  }
+  &__content{
+      margin-left: 300px;
+  }
+}
+</style>
---
+++ ssr/nuxt-test/pages/block/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <article class="article">
+    <div class="content">
+      帝国海军卡萨丁干撒回家打开安顺供电局哈萨克大哥啊是进口德国ask就
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      啊手机打开拉萨角度来看阿斯顿
+    </div>
+    <p class="footer">
+        Last Update:2020/3/14 下午9:20:32
+    </p>
+  </article>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.article {
+  margin: 35px 50px;
+  width: 1200px;
+  .content{
+      padding-bottom: 30px;
+  }
+  .footer{
+      padding-top: 30px;
+      border-top: 1px solid #ddd;
+  }
+}
+</style>
