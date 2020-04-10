c94df5d8c69e2976d1fe054f5c0a8aa7ca71a950
吴昌禄 <1761221407@qq.com>
2020-04-10 12:24:45 +0800

    redux结构设计

--- cloud-music/src/App.js
+++ cloud-music/src/App.js
@@ -4,14 +4,18 @@ import { IconStyle } from "./assets/iconfont/iconfont";
 import { renderRoutes } from "react-router-config";
 import routes from "./routes/index.js";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
