27c7d31439174e087e9700e9c0e8b530560ae215
吴昌禄 <1761221407@qq.com>
2020-04-20 18:23:28 +0800

    歌单详情页面动画添加

--- cloud-music/src/application/Album/indedx.js
+++ cloud-music/src/application/Album/indedx.js
@@ -1,9 +1,34 @@
-//src/application/Album/index.js
-import React from "react";
-import { Container } from "./style";
+//Album/index.js
+import React, { useState, useEffect } from "react";
+import { Container, Animation } from "./style";
+// 先引入
+import Header from "./../../baseUI/header/index";
 
 function Album(props) {
-  return <Container></Container>;
+  const [showStatus, setShowStatus] = useState(false);
+  const [lockStatus, setLockStatus] = useState(true);
+  const handleBack = () => {
+    if (!lockStatus) {
+      setShowStatus(false);
+      setLockStatus(true);
+      setTimeout(() => {
+        props.history.goBack();
+      }, 500);
+    }
+  };
+  useEffect(() => {
+    setShowStatus(true);
+    setLockStatus(false);
+  }, []);
+  return (
+    <Animation>
+      <Container className={showStatus ? "show" : ""}>
+        <Header title={"返回"} handleClick={handleBack}>
+          wqcwqcwq
+        </Header>
+      </Container>
+    </Animation>
+  );
 }
 
-export default Album;
+export default React.memo(Album);
--- cloud-music/src/application/Album/style.js
+++ cloud-music/src/application/Album/style.js
@@ -7,6 +7,15 @@ export const Container = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  z-index: 100;
-  background: #fff;
+  z-index: 1000;
+  background: ${style["background-color"]};
+  transform-origin: right bottom;
+  transition: transform .5s;
+
+  transform: rotatez(30deg) translate3d(100%,0,0);
+`;
+export const Animation = styled.div`
+  .show {
+    transform: rotateZ(0deg) translate3d(0,0,0);
+  }
 `;
